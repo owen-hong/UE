596c945f95666e14ce96b0f4c079c2970ff012bb
UE <520UED.com@gmail.com>
2014-01-24 21:20:43 +0800

    V1.0.0
    V1.0.0

---
+++ other/global.js
@@ -0,0 +1,32 @@
+﻿/*
+*Date: 2013-10-06
+*Email:520ued.com@gmail.com
+*Visit http://www.520ued.com/UE/ for more info.
+*(c) 2013-2014 owenhong, http://www.520ued.com/
+*github:https://github.com/owen-hong/UE
+*versions:1.0.0
+*/
+
+
+
+/*存放本地的JS，这个文件夹大家可以随意存放一些调用UE组件的文件*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
